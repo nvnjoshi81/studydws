2def104b071757da607647eb7cab41d65414c587
naveen <naveen.synsoft@gmail.com>
2020-09-15 19:54:32 +0530

    Update query's

--- application/models/Admin_model.php
+++ application/models/Admin_model.php
@@ -2,14 +2,14 @@
 class Admin_model extends CI_Model
 {
     public function getAdminUsers(){
-        $this->db->select('*');
+        $this->db->select('id,email,password,pass,first_name,last_name,mobile,type,company,address,city,state,postcode,dt_created,dt_modified');
         $this->db->from('admin');
         $this->db->where('type',2);
         $query=$this->db->get();
         return $query->result();
     }
     public function getAdminUser($id){
-        $this->db->select('*');
+        $this->db->select('id,email,password,pass,first_name,last_name,mobile,type,company,address,city,state,postcode,dt_created,dt_modified');
         $this->db->from('admin');
         $this->db->where('id',$id);
         $query=$this->db->get();
@@ -25,7 +25,7 @@ class Admin_model extends CI_Model
     }
    
     public function getUserPermissions($user_id){
-        $this->db->select('*');
+        $this->db->select('id,user_id,item_id,type');
         $this->db->from('cmsuserpermissions');
         $this->db->where('user_id',$user_id);
         $query=$this->db->get();
